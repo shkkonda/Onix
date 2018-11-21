78cd16c89564718e8b156dea235a0dbaa8bbfdc9
shkkonda <shkkonda@Shriharshas-Macbook-Air.local>
2018-11-22 00:54:49 +0530

    Crypto API
    Read data from Coinbase, Kraken, Binance etc. into R

---
+++ cryptoAPI.R
@@ -0,0 +1,12 @@
+#Attempt to read Coinbase data using API
+
+endpoint =
+
+baseurl = "https://api.coinbase.com"
+
+api = paste("/v2", endpoint, sep = "/")
+
+url = paste0(baseurl,api)
+
+api_key = "8ZW5iYaRKaqsLSZw"
+
